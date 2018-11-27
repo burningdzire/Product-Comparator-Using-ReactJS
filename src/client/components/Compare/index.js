bcec3bc4e6e306c65e760f394b6b434148516009
SharmaMohit <mohit.sharma@tatahealth.com>
2018-11-27 21:20:28 +0530

    styling

--- src/client/components/Compare/index.js
+++ src/client/components/Compare/index.js
@@ -5,32 +5,22 @@ const Compare = ({ products }) =>
   <div className="row">
     <div className="col-md-8 text-center">
       <table className="table">
-        <thead className="thead-default">
-          <tr>
-            <th />
-            {products.map(product =>
-              <th key={product.id}>
-                {product.name}
-              </th>
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="price">
-            <th scope="row">Price</th>
-            {products.map(product =>
-              <td key={product.id} className="text-center">{product.price}</td>
-            )}
-          </tr>
-          <tr className="condition">
-            <th scope="row">Condition</th>
-            {products.map(product =>
-              <td key={product.id}>
-                {product.condition}
-              </td>
-            )}
-          </tr>
-        </tbody>
+        <tr>
+          <th />
+          {products.map(product =>
+            <th key={product.id}>
+              {product.name}
+            </th>
+          )}
+        </tr>
+        <tr className="price">
+          <th scope="row">Price</th>
+          {products.map(product => <td key={product.id} className="text-center">{product.price}</td>)}
+        </tr>
+        <tr className="condition">
+          <th scope="row">Condition</th>
+          {products.map(product => <td key={product.id}> {product.condition}</td>)}
+        </tr>
       </table>
     </div>
   </div>;
